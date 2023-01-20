a63c440b4ac44eff5af9b3839b4863fa7376e00f
Jakub Krezolek <kuborek2@gmail.com>
2023-01-20 12:11:45 +0100

    Added redux toolkit dependencies of redux and redux toolkit as well as default router and store was set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Provider } from 'react-redux'
+import store from './store/storeIndex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ frontend/src/store/storeIndex.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import loginSlice from './loginSlice';
+
+export default configureStore({
+    reducer: {
+      login: loginSlice,
+
+    }
+  });
